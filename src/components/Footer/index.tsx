e861a503f639c93d112ad895ee820c450e8b5c93
PrisacariAndrei <andrei.prisacari@fuselabcreative.email>
2022-12-20 15:11:46 +0200

    created desktop version of footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,129 @@
+import Facebook from '../../images/Facebook'
+import { AngelList, Dribble, GitHub, LinkedIn, Twitter } from '../../images'
+
+const Footer = () => {
+    return (
+        <div className="pb-19px text-white pt-[78px]">
+            <div className="gap-[125px] flex flex-row pb-[39px]">
+                <div className="flex flex-col items-start gap-[17px]">
+                    <div className="font-bold text-[50.52px] leading-[61px]">
+                        <span className="header-logo-gradient">Apollo</span>
+                        <span className="text-secondary-main">NFT</span>
+                    </div>
+                    <div className="font-['Inter'] font-normal text-[18px] leading-[18px] text-[#999999] not-italic w-[279px]">
+                        This growth plan will help you reach your resolutions
+                        and achieve the goals you have been striving towards.
+                    </div>
+                    <div className="flex flex-row items-center gap-[24px]">
+                        <Twitter
+                            width="24px"
+                            height="24px"
+                            className="fill-gray-400"
+                        />
+                        <LinkedIn
+                            width="24px"
+                            height="24px"
+                            className="fill-gray-400"
+                        />
+                        <Facebook
+                            width="24px"
+                            height="24px"
+                            className="fill-gray-400"
+                        />
+                        <GitHub
+                            width="24px"
+                            height="24px"
+                            className="fill-gray-400"
+                        />
+                        <AngelList
+                            width="24px"
+                            height="24px"
+                            className="fill-gray-400"
+                        />
+                        <Dribble
+                            width="24px"
+                            height="24px"
+                            className="fill-gray-400"
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-row justify-between w-full ">
+                    <div className="flex flex-col items-start">
+                        <div className="pb-[20px] text-white font-semibold text-[16px] leading-[19px]">
+                            Marketplace
+                        </div>
+                        <div className="flex flex-col gap-[9px]">
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Buy Product
+                            </div>
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Sell Product
+                            </div>
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Our Creator
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col items-start">
+                        <div className="pb-[20px] text-white font-semibold text-[16px] leading-[19px]">
+                            Resources
+                        </div>
+                        <div className="flex flex-col gap-[9px]">
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                About Us
+                            </div>
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Event
+                            </div>
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Tutorial
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col items-start">
+                        <div className="pb-[20px] text-white font-semibold text-[16px] leading-[19px]">
+                            Company
+                        </div>
+                        <div className="flex flex-col gap-[9px]">
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Media
+                            </div>
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Blog
+                            </div>
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Pricing
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col items-start">
+                        <div className="pb-[20px] text-white font-semibold text-[16px] leading-[19px]">
+                            Legal
+                        </div>
+                        <div className="flex flex-col gap-[9px]">
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Terms
+                            </div>
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Privacy
+                            </div>
+                            <div className="text-[#999999] font-normal text-[15px] leading-[18px]">
+                                Support
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-row justify-center items-center pt-8 gap-8 border-t border-t-inner-border">
+                <span className="text-gray-400 font-normal text-base">
+                    © 2077 ApolloNFT. All rights reserved.
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/images/index.tsx
@@ -0,0 +1,8 @@
+import Facebook from './Facebook'
+import GitHub from './GitHub'
+import AngelList from './AngelList'
+import LinkedIn from './LinkedIn'
+import Twitter from './Twitter'
+import Dribble from './Dribble'
+
+export { Facebook, GitHub, AngelList, LinkedIn, Twitter, Dribble }
