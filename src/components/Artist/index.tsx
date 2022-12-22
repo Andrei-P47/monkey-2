34763f2b846edefd6d43da3c86c033f5d2b97c9b
PrisacariAndrei <andrei.prisacari@corlab.tech>
2022-12-22 23:34:03 +0200

    created static desktop version of live-aucition

--- src/components/Artist/index.tsx
+++ src/components/Artist/index.tsx
@@ -1,28 +1,30 @@
 const Artist = () => {
     return (
-        <div className="flex flex-row items-start gap-[29px] text-white mt-[30px] mb-[32px]">
-            <div className="flex flex-col items-start gap-2">
-                <div className="font-normal text-[16px] leading-[19px] tracking-[.003em]">
-                    Artwork
+        <div className="container mx-auto w-[1208px]">
+            <div className="flex flex-row items-start gap-[29px] text-white mt-[30px] mb-[32px]">
+                <div className="flex flex-col items-start gap-2">
+                    <div className="font-normal text-[16px] leading-[19px] tracking-[.003em]">
+                        Artwork
+                    </div>
+                    <div className="font-bold text-[28px] leading-[34px]">
+                        25.1k
+                    </div>
                 </div>
-                <div className="font-bold text-[28px] leading-[34px]">
-                    25.1k
+                <div className="flex flex-col items-start gap-2">
+                    <div className="font-normal text-[16px] leading-[19px] tracking-[.003em]">
+                        Artist
+                    </div>
+                    <div className="font-bold text-[28px] leading-[34px]">
+                        15.6k
+                    </div>
                 </div>
-            </div>
-            <div className="flex flex-col items-start gap-2">
-                <div className="font-normal text-[16px] leading-[19px] tracking-[.003em]">
-                    Artist
-                </div>
-                <div className="font-bold text-[28px] leading-[34px]">
-                    15.6k
-                </div>
-            </div>
-            <div className="flex flex-col items-start gap-2">
-                <div className="font-normal text-[16px] leading-[19px] tracking-[.003em]">
-                    Aucition
-                </div>
-                <div className="font-bold text-[28px] leading-[34px]">
-                    10.2k
+                <div className="flex flex-col items-start gap-2">
+                    <div className="font-normal text-[16px] leading-[19px] tracking-[.003em]">
+                        Aucition
+                    </div>
+                    <div className="font-bold text-[28px] leading-[34px]">
+                        10.2k
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -5,60 +5,64 @@ import third from './../../images/HeroCardsImage/3.jpg'
 
 const HeroSection = () => {
     return (
-        <div className="flex flex-row justify-between mt-[120px]">
-            <div className="flex flex-col gap-[30px] w-[726px]">
-                <div className="font-extrabold text-[94px] leading-[90px] text-white bor whitespace-pre ">
-                    <div>Create Your </div>
-                    Own&nbsp;
-                    <span className="hero-title-gradient flex-none">
-                        NFT Dream
-                    </span>
-                    <div className="flex flex-row">
-                        Gallery
-                        <img
-                            className="mt-4"
-                            src={star}
-                            width={85}
-                            height={85}
-                            alt="star"
-                        />
-                    </div>
-                </div>
-                <div className="flex flex-row gap-[30px]">
-                    <div className="flex flex-row justify-center items-center py-[47px] px-[10px] gap-2.5 rounded-[200px] discover-nft-button-gradient">
-                        <span className="font-semibold text-[16px] leading-[26px] text-white flex-none">
-                            Discover NFT
+        <div className="container mx-auto w-[1208px]">
+            <div className="flex flex-row justify-between mt-[120px]">
+                <div className="flex flex-col gap-[30px] w-[726px]">
+                    <div className="font-extrabold text-[94px] leading-[90px] text-white bor whitespace-pre ">
+                        <div>Create Your </div>
+                        Own&nbsp;
+                        <span className="hero-title-gradient flex-none">
+                            NFT Dream
                         </span>
+                        <div className="flex flex-row">
+                            Gallery
+                            <img
+                                className="mt-4"
+                                src={star}
+                                width={85}
+                                height={85}
+                                alt="star"
+                            />
+                        </div>
                     </div>
-                    <div className="font-normal text-[16px] leading-[26px] text-white">
-                        The Larges NFT Marketplace. Automatic and truly <br />
-                        unique digital creation. Signed and issued by the <br />
-                        creator, made possible by blockchain technologi
+                    <div className="flex flex-row gap-[30px]">
+                        <div className="flex flex-row justify-center items-center py-[47px] px-[10px] gap-2.5 rounded-[200px] discover-nft-button-gradient">
+                            <span className="font-semibold text-[16px] leading-[26px] text-white flex-none">
+                                Discover NFT
+                            </span>
+                        </div>
+                        <div className="font-normal text-[16px] leading-[26px] text-white">
+                            The Larges NFT Marketplace. Automatic and truly{' '}
+                            <br />
+                            unique digital creation. Signed and issued by the{' '}
+                            <br />
+                            creator, made possible by blockchain technologi
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="relative top-0 right-0">
-                <img
-                    className="rounded-[24px] relative z-40"
-                    src={first}
-                    width={388}
-                    height={463}
-                    alt={first}
-                />
-                <img
-                    className="rounded-[24px] absolute top-[-55px] left-[-60px]"
-                    src={second}
-                    width={366}
-                    height={437}
-                    alt={second}
-                />
-                <img
-                    className="rounded-[24px] absolute bottom-[-60px] right-[-60px]"
-                    src={third}
-                    width={341}
-                    height={407}
-                    alt={third}
-                />
+                <div className="relative top-0 right-0">
+                    <img
+                        className="rounded-[24px] relative z-40"
+                        src={first}
+                        width={388}
+                        height={463}
+                        alt={first}
+                    />
+                    <img
+                        className="rounded-[24px] absolute top-[-55px] left-[-60px]"
+                        src={second}
+                        width={366}
+                        height={437}
+                        alt={second}
+                    />
+                    <img
+                        className="rounded-[24px] absolute bottom-[-60px] right-[-60px]"
+                        src={third}
+                        width={341}
+                        height={407}
+                        alt={third}
+                    />
+                </div>
             </div>
         </div>
     )
