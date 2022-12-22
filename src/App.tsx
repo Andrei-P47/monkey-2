945bdc49279eaf95d99f69f35126d5ff978069cf
PrisacariAndrei <andrei.prisacari@fuselabcreative.email>
2022-12-22 09:17:57 +0200

    created static desktop version of join our community

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
 } from './components'
 import CreateYourNft from './components/CreateYourNft'
 import CreatorOfTheWeek from './components/CreatorOfTheWeek'
+import JoinOurCommunity from './components/JoinOurCommunity'
 
 function App() {
     return (
@@ -23,6 +24,7 @@ function App() {
                 {/*<Aici trebuie sa mai fie ceva*/}
                 <CreateYourNft />
                 <CreatorOfTheWeek />
+                <JoinOurCommunity />
                 <Footer />
             </div>
         </div>
